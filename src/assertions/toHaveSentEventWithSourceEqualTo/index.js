48dda97d9a59a1d5befaabeef4fd88de6e2c0185
Sarah Hamilton <sarahh@theodo.co.uk>
2021-04-29 16:08:58 +0100

    Add in eslint and prettier

--- src/assertions/toHaveSentEventWithSourceEqualTo/index.js
+++ src/assertions/toHaveSentEventWithSourceEqualTo/index.js
@@ -1,18 +1,17 @@
 export default {
   toHaveSentEventWithSourceEqualTo(sqsResult, expectedSourceName) {
     const receivedSource = JSON.parse(sqsResult.Messages[0].Body).source;
-    if (receivedSource == expectedSourceName) {
+    if (receivedSource === expectedSourceName) {
       return {
         message: () =>
           `expected sent event to have source ${expectedSourceName}`,
         pass: true,
       };
-    } else {
-      return {
-        message: () =>
-          `sent event source "${receivedSource}" does not match expected source "${expectedSourceName}"`,
-        pass: false,
-      };
     }
+    return {
+      message: () =>
+        `sent event source "${receivedSource}" does not match expected source "${expectedSourceName}"`,
+      pass: false,
+    };
   },
 };
