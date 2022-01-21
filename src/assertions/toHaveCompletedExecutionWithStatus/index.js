6371edf7cedbf1745b9bc6f92bd5fa8fc7b6b396
joel-aleios <joelh@aleios.com>
2022-01-21 11:53:44 +0000

    Get toHaveCompletedExecutionWithStatus() assertion working.

---
+++ src/assertions/toHaveCompletedExecutionWithStatus/index.js
@@ -0,0 +1,36 @@
+import { testResult } from "../../utils/testResult";
+
+const AWS = require("aws-sdk");
+
+export default {
+  async toHaveCompletedExecutionWithStatus(stateMachineName, expectedStatus) {
+    const stepFunctions = new AWS.StepFunctions();
+    const listStateMachineParams = {};
+    // Get all state machines
+    const allStateMachines = await stepFunctions
+      .listStateMachines(listStateMachineParams)
+      .promise();
+    // Find state machine with specified name and get its arn
+    const smList = allStateMachines.stateMachines.filter(
+      (stateMachine) => stateMachine.name === stateMachineName
+    );
+    const smArn = smList[0].stateMachineArn;
+    const listExecutionsParams = { stateMachineArn: smArn };
+    // Get all executions of specified state machine
+    const smExecutions = await stepFunctions
+      .listExecutions(listExecutionsParams)
+      .promise();
+    // Get the latest execution (list ordered in reverse chronological)
+    const latestExecution = smExecutions.executions[0];
+    if (latestExecution.status === expectedStatus) {
+      return testResult(
+        `Execution status is ${expectedStatus}, as expected.`,
+        true
+      );
+    }
+    return testResult(
+      `Execution status was ${latestExecution.status}, where it was expected to be ${expectedStatus}`,
+      false
+    );
+  },
+};
