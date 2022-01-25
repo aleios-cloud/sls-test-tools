0558de3e5868ab1c2964162f0c1939f201558167
joel-aleios <joelh@aleios.com>
2022-01-25 17:14:47 +0000

    Added assertion for expected state machine output.

--- src/assertions/index.ts
+++ src/assertions/index.ts
@@ -6,6 +6,7 @@ import toHaveEventWithSource from "./toHaveEventWithSource";
 import toHaveObjectWithNameEqualTo from "./toHaveObjectWithNameEqualTo";
 import toExistInDynamoTable from "./toExistInDynamoTable";
 import toHaveCompletedExecutionWithStatus from "./toHaveCompletedExecutionWithStatus";
+import toMatchStateMachineOutput from "./toMatchStateMachineOutput";
 
 export default {
   ...toExistAsS3Bucket,
@@ -15,5 +16,6 @@ export default {
   ...toHaveEventWithSource,
   ...toHaveObjectWithNameEqualTo,
   ...toExistInDynamoTable,
-  ...toHaveCompletedExecutionWithStatus
+  ...toHaveCompletedExecutionWithStatus,
+  ...toMatchStateMachineOutput,
 };
