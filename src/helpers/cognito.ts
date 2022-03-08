ed462a5e3397526e2ec68acc7662a4a2c6dd7ba8
joel-aleios <joelh@aleios.com>
2022-03-08 18:02:15 +0000

    create authenticated user and create unauthenticated user helpers started.

--- src/helpers/cognito.ts
+++ src/helpers/cognito.ts
@@ -1,16 +1,67 @@
 import { CognitoIdentityServiceProvider } from "aws-sdk";
 import { AWSClient } from "./general";
+import { Chance } from "chance";
 
-export const createUserInUserPoolWithUsername = async (
-  userPoolId: string,
-  username: string
-): Promise<CognitoIdentityServiceProvider.AdminCreateUserResponse> => {
+export const createUnauthenticatedUser = async (
+  clientId: string
+): Promise<CognitoIdentityServiceProvider.SignUpResponse> => {
   const cognitoClient: CognitoIdentityServiceProvider = new AWSClient.CognitoIdentityServiceProvider();
+  const chance = new Chance();
+  const username = chance.email();
 
   return await cognitoClient
-    .adminCreateUser({
+    .signUp({
+      ClientId: clientId,
+      Username: username,
+      Password: chance.string({ length: 8 }),
+    })
+    .promise();
+};
+
+class AuthenticatedUser {}
+
+export const createAuthenticatedUser = async (
+  clientId: string,
+  userPoolId: string
+): Promise<CognitoIdentityServiceProvider.AdminConfirmSignUpResponse> => {
+  const cognitoClient: CognitoIdentityServiceProvider = new AWSClient.CognitoIdentityServiceProvider();
+  const chance = new Chance();
+  const username: string = chance.email();
+  const password: string = chance.string({ length: 8 });
+
+  //   const signUpResponse: CognitoIdentityServiceProvider.SignUpResponse =
+  const res: CognitoIdentityServiceProvider.SignUpResponse = await cognitoClient
+    .signUp({
+      ClientId: clientId,
+      Username: username,
+      Password: password,
+    })
+    .promise();
+
+  console.log(res);
+
+  await cognitoClient
+    .adminConfirmSignUp({
       UserPoolId: userPoolId,
       Username: username,
     })
     .promise();
+
+  const auth: CognitoIdentityServiceProvider.InitiateAuthResponse = await cognitoClient
+    .initiateAuth({
+      AuthFlow: "USER_PASSWORD_AUTH",
+      ClientId: clientId,
+      AuthParameters: {
+        USERNAME: username,
+        PASSWORD: password,
+      },
+    })
+    .promise();
+
+  return {
+    username,
+    password,
+    idToken: auth.AuthenticationResult.IdToken,
+    accessToken: auth.AuthenticationResult.AccessToken,
+  };
 };
