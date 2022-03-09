27632f07a6a9ed9578a9e03d5efe3a3fe80dd89f
joel-aleios <joelh@aleios.com>
2022-03-09 16:43:02 +0000

    end of day commit

--- src/helpers/cognito.ts
+++ src/helpers/cognito.ts
@@ -2,66 +2,120 @@ import { CognitoIdentityServiceProvider } from "aws-sdk";
 import { AWSClient } from "./general";
 import { Chance } from "chance";
 
-export const createUnauthenticatedUser = async (
-  clientId: string
-): Promise<CognitoIdentityServiceProvider.SignUpResponse> => {
+interface User {
+  username: string;
+  password: string;
+  confirmed?: boolean | undefined;
+}
+
+interface CreateUserInput {
+  clientId: string;
+  userPoolId: string;
+  confirmed: boolean;
+}
+
+interface ConfirmUserInput {
+  userPoolId: string;
+  username: string;
+  password: string;
+}
+
+const createUser = async (
+  clientId: string,
+  username: string
+): Promise<User> => {
   const cognitoClient: CognitoIdentityServiceProvider = new AWSClient.CognitoIdentityServiceProvider();
   const chance = new Chance();
-  const username = chance.email();
-
-  return await cognitoClient
+  const password: string = chance.string({ length: 8 });
+  await cognitoClient
     .signUp({
       ClientId: clientId,
       Username: username,
       Password: chance.string({ length: 8 }),
     })
     .promise();
+  console.log({ username, password });
+
+  return {
+    username,
+    password,
+  };
 };
 
-class AuthenticatedUser {}
+const confirmUser = async (input: ConfirmUserInput): Promise<User> => {
+  const cognitoClient: CognitoIdentityServiceProvider = new AWSClient.CognitoIdentityServiceProvider();
+
+  await cognitoClient
+    .adminConfirmSignUp({
+      UserPoolId: input.userPoolId,
+      Username: input.username,
+    })
+    .promise();
+
+  console.log({ username: input.username, password: input.password });
+
+  return {
+    username: input.username,
+    password: input.password,
+    confirmed: true,
+  };
+};
+
+export const createUnauthenticatedUser = async (
+  input: CreateUserInput
+): Promise<User> => {
+  const chance = new Chance();
+  const username: string = chance.email();
+  const user: User = await createUser(input.clientId, username);
+
+  if (input.confirmed) {
+    return await confirmUser({
+      userPoolId: input.userPoolId,
+      username: username,
+      password: user.password,
+    });
+  }
+
+  return {
+    username: username,
+    password: user.password,
+    confirmed: input.confirmed,
+  };
+};
 
 export const createAuthenticatedUser = async (
-  clientId: string,
-  userPoolId: string
+  input: CreateUserInput
 ): Promise<CognitoIdentityServiceProvider.AdminConfirmSignUpResponse> => {
   const cognitoClient: CognitoIdentityServiceProvider = new AWSClient.CognitoIdentityServiceProvider();
   const chance = new Chance();
   const username: string = chance.email();
-  const password: string = chance.string({ length: 8 });
 
-  //   const signUpResponse: CognitoIdentityServiceProvider.SignUpResponse =
-  const res: CognitoIdentityServiceProvider.SignUpResponse = await cognitoClient
-    .signUp({
-      ClientId: clientId,
-      Username: username,
-      Password: password,
-    })
-    .promise();
+  const user: User = await createUser(input.clientId, username);
+  console.log({ username });
 
-  console.log(res);
+  await confirmUser({
+    userPoolId: input.userPoolId,
+    username: username,
+    password: user.password,
+  });
 
-  await cognitoClient
-    .adminConfirmSignUp({
-      UserPoolId: userPoolId,
-      Username: username,
-    })
-    .promise();
+  console.log({ username: user.username, password: user.password });
 
-  const auth: CognitoIdentityServiceProvider.InitiateAuthResponse = await cognitoClient
-    .initiateAuth({
+  const auth: CognitoIdentityServiceProvider.AdminInitiateAuthResponse = await cognitoClient
+    .adminInitiateAuth({
       AuthFlow: "USER_PASSWORD_AUTH",
-      ClientId: clientId,
+      ClientId: input.clientId,
       AuthParameters: {
         USERNAME: username,
-        PASSWORD: password,
+        PASSWORD: user.password,
       },
     })
     .promise();
 
   return {
     username,
-    password,
-    idToken: auth.AuthenticationResult.IdToken,
-    accessToken: auth.AuthenticationResult.AccessToken,
+    password: user.password,
+    idToken: auth.AuthenticationResult?.IdToken,
+    accessToken: auth.AuthenticationResult?.AccessToken,
   };
 };
